58ee19b14deb05f05b2e29b2a90fd5393e1be9a0
lxbachit03 <lxbachit03.working@gmail.com>
2025-02-15 15:42:30 +0700

    config typescript and nativewind (tailwind) CSS

---
+++ apps/client/app/_layout.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Slot } from "expo-router";
+import "../global.css";
+
+const RootLayout = () => {
+  return (
+    <>
+      <Text>Header</Text>
+      <Slot />
+      <Text className="text-red-600">Footer</Text>
+    </>
+  );
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({});
---
+++ apps/client/app/index.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-blue-700">
+        Open up App.js to start working on your app! asdasdasdasd
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
