421c32c5be35cfb74f7db7bb966bc6a2beb87b58
lxbachit03 <lxbachit03.working@gmail.com>
2025-03-01 14:51:05 +0700

    UI products screen

--- client/src/app/products/index.tsx
+++ client/src/app/products/index.tsx
@@ -1,11 +1,33 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import ProductLayout from "~/components/layouts/product.layout";
+import ProductItem from "@components/ui/product-item";
+import { router } from "expo-router";
 
 const ProductScreen = () => {
   return (
-    <View>
-      <Text>ProductScreen</Text>
-    </View>
+    <ProductLayout className="">
+      <Text className="mb-10">Product Screen</Text>
+      <View className="flex flex-row flex-wrap items-center justify-center gap-6">
+        {[1, 2, 3, 4].map((item, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              onPress={() => {
+                router.push("/products/lamerei");
+              }}
+            >
+              <ProductItem
+                title="lamerei"
+                description="reversible angora cardigan"
+                price={120}
+                imageUrl="https://res.cloudinary.com/djiju7xcq/image/upload/v1729839380/Sunflower-Jumpsuit-1-690x875_dibawa.webp"
+              />
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    </ProductLayout>
   );
 };
 
