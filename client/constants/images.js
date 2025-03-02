809638a6fb473c33f4b0dc3c63743aeb4ed846e7
tqphuong3011 <122359573+tqphuong3011@users.noreply.github.com>
2025-03-02 23:16:42 +0700

    UI onboarding

--- client/constants/images.js
+++ client/constants/images.js
@@ -6,4 +6,7 @@ export default {
   footer02: require("assets/images/footer02.png"),
   footer03: require("assets/images/footer03.png"),
   footer04: require("assets/images/footer04.png"),
+  onboarding01: require("assets/images/onboarding01.png"),
+  onboarding02: require("assets/images/onboarding02.png"),
+  onboarding03: require("assets/images/onboarding03.png"),
 };
--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -1,49 +1,103 @@
 import "expo-router/entry";
 
 import { Link, router } from "expo-router";
-import { ScrollView, View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import Button from "~/components/ui/Button";
+import CarouselOnboarding from "@components/ui/carousel-orboarding";
+import { images, svgIcons } from "~/constants";
 
 const App = () => {
-  return (
-    <SafeAreaView className="bg-[#0D0D0D] h-full">
-      <ScrollView
-        contentContainerStyle={{
-          height: "100%",
-        }}
-      >
-        <View className="flex items-center justify-center h-full">
-          <View className="mb-32">
+
+  const onboardingItems = [
+    {
+      id:"1",
+      content: (
+        <View className="h-full flex justify-end items-center pb-[100px]">
+          <Image
+            source={images.onboarding01}
+            style={{ width: "100%", height: 655, resizeMode: "contain" }} 
+          />
+        </View>
+      ),
+    },
+    {
+      id: "2",
+      content: (
+        <View className="h-full flex justify-end items-center pb-[100px]">
+          <svgIcons.OctoberCollectionSvg
+            width={200}
+            height={150}
+          />
+          <Image
+            source={images.onboarding02}
+            style={{ width: "100%", height: 550, resizeMode: "contain" }} 
+          />
+        </View>
+      ),
+    },
+    {
+      id: "3",
+      content: (
+        <View className="h-full flex justify-end items-center pb-[100px]">
+          <svgIcons.OctoberCollectionSvg
+            width={200}
+            height={150}
+          />
+          <Image
+            source={images.onboarding03}
+            style={{ width: "100%", height: 550, resizeMode: "contain" }} 
+          />
+        </View>
+      ),
+    },
+    {
+      id: "4",
+      content: 
+        <View className="h-full flex justify-center items-center gap-5 pb-[100px]">
+          {/* <View className="mb-32">
             <Text className="text-xl font-semibold text-white font-Poppins-SemiBold">
               On boarding Screen
             </Text>
-          </View>
-
+          </View> */}
+          <svgIcons.LogoIcon
+            width={300}
+            height={200}
+          />
           <Button
-            title="Login to continue login"
-            className="bg-blue-950"
+            title="LOGIN TO APP"
+            className="bg-white w-[300px] py-5 rounded-md"
+            textStyles="font-TenorSans-Regular text-lg"
             onPress={() => {
               router.push("/sign-in");
             }}
           />
 
           <Button
-            title="Login to continue register"
-            className="mt-5 bg-blue-950"
+            title="CONTINUE WITHOUT LOGIN"
+            className="bg-gray-400 w-[300px] py-5 rounded-md"
+            textStyles="font-TenorSans-Regular text-lg"
             onPress={() => {
-              router.push("/sign-up");
+              router.push("/home");
             }}
           />
-
-          <View className="mt-5">
-            <Link href={"/home"} className="text-white border-b border-white">
-              Continue without -&gt;
-            </Link>
-          </View>
+        </View>,
+    },
+  ];
+  // 0D0D0D
+  //
+  return (
+    <SafeAreaView className="bg-[#111111] h-full">
+      <View className="h-full w-full">
+        <View className="flex items-center justify-center h-full">
+          <CarouselOnboarding
+            items={onboardingItems.map((item) => {
+              return (item.content);
+            })}
+          />
         </View>
-      </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
